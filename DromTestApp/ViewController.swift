cb8ed8540e1362c344008bcba20a19d18732d913
Vladimir Voloboev <vladimir.voloboev@digital-ecosystems.ru>
2022-05-21 00:03:45 +0700

    Initial Commit

---
+++ DromTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DromTestApp
+//
+//  Created by Владимир on 21.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
